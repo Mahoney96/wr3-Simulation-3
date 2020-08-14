4403ba058f7df59b95dca61aa4a4e7e4bb24b0f5
Mahoney96 <cmahoney966@gmail.com>
2020-08-14 14:23:54 -0700

    2:23Pm

--- server/index.js
+++ server/index.js
@@ -1,26 +1,32 @@
 require('dotenv').config();
 const express = require('express');
-const session = require('massive');
+// const session = require('express-session');
 const massive = require('massive');
-
 const app = express();
 
 
-const PORT = 3993;
+// const port = 3993;
 
 const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env;
+
+app.use(express.json());
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {maxAge: 1000 * 60 * 60 * 24},
+//     secret: SESSION_SECRET
+// }))
 
 massive({
     connectionString: CONNECTION_STRING,
     ssl: {
         rejectUnathorized: false
     }
-}).then((dbInstance => {
-    app.set('db', dbinstance);
-}));
+}).then(db => {
+    app.set('db', db)
+    console.log('connected to db')
+}).catch(err => console.log(err));
 
 
-
-app.use(express.json());
 
 app.listen(PORT, () => console.log(`Hey Hey, Your Server Works Today ${PORT}`));
